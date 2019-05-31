e344b8a49a86f7f815e95a5dafd4eae969e15f25
Jack <48194176+jharmon96@users.noreply.github.com>
2019-05-31 13:52:27 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,27 +1,32 @@
 from os import sys, path
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-
 import time
-import copy_cost_structure
+import cost_structure
 import standard_functions
+import user_input
 import settings
 from pynput.keyboard import Controller
 
 # Main.py kicks off the script
 
+user_input.prompt_credentials()
+user_input.prompt_direction()
+user_input.prompt_details()
+
 
 # initialize keyboard controller
 settings.keyboard = Controller()
-
-# initialize web browser used to retrieve / input information
-standard_functions.initialize_browser()
 
 # call the login function
 standard_functions.login()
 
 # Kick off process
-copy_cost_structure.navigate()
+cost_structure.run()
+
+# Test #
+# export_cost_structure.test()
+
 
 # exit out of web browser
 time.sleep(1)
